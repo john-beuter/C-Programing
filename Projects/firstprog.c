63ccc6779f3b63e1a2322f192ca1cf49a697da23
Johnathon Beuter <jbeuter@iastate.edu>
2024-03-09 17:14:17 -0600

    adding code

---
+++ Projects/firstprog.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    for(i = 0; i < 10; i++)
+    {
+        puts("Hello, world!\n"); //Puts the string as output
+    }
+    return 0; //Exit the program
+}
