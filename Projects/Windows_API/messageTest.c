060c49cb98699cb24ca647acea54dc80b2e22205
Johnathon Beuter <jbeuter@iastate.edu>
2024-03-15 20:32:25 -0500

    Got some good changes made

--- Projects/Windows_API/messageTest.c
+++ Projects/Windows_API/messageTest.c
@@ -1,49 +1,67 @@
 #include <windows.h> //Houses windows api functions we can use
 #include <stdio.h> //Allows me to print
+//#include <sys/wait.h> Windows does not have wait
 
 //Reference Video: https://www.youtube.com/watch?v=aNEqC-U5tHM
 
 int main(void)
 {
+    // int MessageBox(
+    // [in, optional] HWND    hWnd,
+    // [in, optional] LPCTSTR lpText,
+    // [in, optional] LPCTSTR lpCaption,
+    // [in]           UINT    uType
+    // );
+
     //This is a lot like the visual basic script I wrote
     MessageBoxW(
         NULL,
-        L"100 Malicious Files Found", //Message displayed
-        L"COMPUTE HAS BEEN INFECTED", //Message header
-        MB_YESNOCANCEL | MB_ICONEXCLAMATION //Specifies that we want a yes, no, cancel
+        L"This process will take: 3 HOURS", //Message displayed
+        L"Warning", //Message header
+        MB_YESNO | MB_ICONWARNING //Specifies that we want a yes, no, cancel
         //Outputs with an exclamation
     );
 
+    //
     // BOOL CreateProcessA(
-    // [in, optional]      LPCSTR                lpApplicationName,
-    // [in, out, optional] LPSTR                 lpCommandLine,
-    // [in, optional]      LPSECURITY_ATTRIBUTES lpProcessAttributes,
-    // [in, optional]      LPSECURITY_ATTRIBUTES lpThreadAttributes,
-    // [in]                BOOL                  bInheritHandles,
-    // [in]                DWORD                 dwCreationFlags,
-    // [in, optional]      LPVOID                lpEnvironment,
-    // [in, optional]      LPCSTR                lpCurrentDirectory,
-    // [in]                LPSTARTUPINFOA        lpStartupInfo,
-    // [out]               LPPROCESS_INFORMATION lpProcessInformation
+    // [in, optional]      LPCSTR                lpApplicationName, In this case we are running notepad
+    // [in, out, optional] LPSTR                 lpCommandLine,     Not needed here
+    // [in, optional]      LPSECURITY_ATTRIBUTES lpProcessAttributes,   Not needed here
+    // [in, optional]      LPSECURITY_ATTRIBUTES lpThreadAttributes,    Not needed here
+    // [in]                BOOL                  bInheritHandles,       Not needed
+    // [in]                DWORD                 dwCreationFlags,       What level do we want this process to run as?
+    // [in, optional]      LPVOID                lpEnvironment,         Not needed
+    // [in, optional]      LPCSTR                lpCurrentDirectory,    Not needed
+    // [in]                LPSTARTUPINFOA        lpStartupInfo,         Needs a struct to this
+    // [out]               LPPROCESS_INFORMATION lpProcessInformation   
     // );
 
-    STARTUPINFO si = { 0 };
-    PROCESS_INFORMATION info = { 0 };
+    STARTUPINFOW si = { 0 }; //Specifies unicode, gives the data a uniform formate so it can be understood by other computers
+    PROCESS_INFORMATION info = { 0 }; //Had some issues with the formating here. In Windows API there is some different variable types that all looks similar
+    //LPPROCESS is different than PROCESS 
+    
 
-    if(!CreateProcessW( //Spawns a notepad instance!!
-        L"C:\\Windows\\System32\\notepad.exe",
-        NULL,
-        NULL,
-        NULL,
-        FALSE,
-        BELOW_NORMAL_PRIORITY_CLASS,
-        NULL,
-        NULL,
-        &si,
-        &info))
+    if(MK_SHIFT) //When the left click button is pressed spawn a process. 
     {
-        printf("(-) failed to create a process, error %ld", GetLastError()); //Retrieves the calling threads error code
+            if(!CreateProcessW( //Spawns a notepad instance!!
+            L"C:\\Windows\\System32\\notepad.exe",
+            NULL,
+            NULL,
+            NULL,
+            FALSE,
+            BELOW_NORMAL_PRIORITY_CLASS,
+            NULL,
+            NULL,
+            &si,
+            &info))
+            {
+                printf("(-) failed to create a process, error %ld", GetLastError()); //Retrieves the calling threads error code
+            }
+            printf("(+) process running! pid: %ld", info.dwProcessId);
     }
-
+    else if(WM_LBUTTONDOWN)
+    {
+         return 0;
+    }
     return EXIT_SUCCESS; 
 }
