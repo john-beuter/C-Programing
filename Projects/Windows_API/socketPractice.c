78e75b14b86a541ddfcbf37537c8eebd3d9cccea
Johnathon Beuter <jbeuter@iastate.edu>
2024-03-28 22:31:56 -0500

    Making socket example

---
+++ Projects/Windows_API/socketPractice.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <winsock2.h>
+
+int main()
+{
+    int i;
+    for(i = 0; i < 10; i++)
+    {
+        puts("Hello, world!\n"); //Puts the string as output
+    }
+    return 0; //Exit the program
+}
