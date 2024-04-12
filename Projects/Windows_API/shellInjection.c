7e03effc6401d2b1640eb49636a138c7b2a7ab69
Johnathon Beuter <jbeuter@iastate.edu>
2024-04-12 12:45:39 -0500

    Making a shell code injection

--- Projects/Windows_API/shellInjection.c
+++ Projects/Windows_API/shellInjection.c
@@ -7,11 +7,9 @@ const char * i = "[*]";
 
 int main(int argc, char * argv[])
 {
-    void * buffer = NULL;
-    DWORD pid = NULL; //32 bit unsigned integer
-    DWORD tid = NULL;
-    HANDLE process = NULL;
-    HANDLE thread = NULL;
+    PVOID rBuffer = NULL;
+    DWORD dwPID = NULL, dwTID = NULL;
+    HANDLE hProcess = NULL, hThread = NULL; 
 
     if(argc < 2)
     {
@@ -19,8 +17,35 @@ int main(int argc, char * argv[])
         return EXIT_FAILURE;
     }
 
-    pid = atoi(argv[1]);
-    printf("%s trying to get a handle to the process (%ld)\n", i, pid);
+    dwPID = atoi(argv[1]);
+    printf("%s trying to get a handle to the process (%ld)\n", i, dwPID);
+
+    hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, dwPID); //Privs and PID of proocess we want handle to 
+    if(hProcess == NULL)
+    {
+        printf("failed to get process handle, error: 0x%1x", GetLastError()); //GetLastError is from Microsoft. Value is the calling threads last error code.
+        return EXIT_FAILURE;
+    }
+
+    unsigned char test[] = "\x61\x61\x61\x61\x61\x61";
+    size_t testSize = sizeof(test);
+
+    //Create a space for our process to exist in 
+    rBuffer = VirtualAllocEx(hProcess, NULL, testSize, (MEM_RESERVE | MEM_COMMIT), PAGE_EXECUTE_READWRITE); // (MEM_RESERVE | MEM_COMMIT) allows you to do both
+    //Making permission rw first then once it is established (made), use virtualprotect() to make it read write execute
+    //Use documenation to do this with virtual protect
+
+    if(rBuffer == NULL)
+    {
+        printf("%s failed to get allocate buffer, error: 0x%1x", k, GetLastError());
+        return EXIT_FAILURE;
+    }
+
+
+    //Make our process happen
+    WriteProcessMemory(hProcess, rBuffer, test, testSize, NULL);
+    printf("%s wrote shellcode to the buffer\n", k);
+
 
     return EXIT_SUCCESS;
 }
