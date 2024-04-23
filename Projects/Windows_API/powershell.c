abcf1c90c5f4d9611ba56fb721f0f1c08495df40
Johnathon Beuter <jbeuter@iastate.edu>
2024-04-23 15:43:04 -0500

    Made some more stealh changes

--- Projects/Windows_API/powershell.c
+++ Projects/Windows_API/powershell.c
@@ -3,7 +3,7 @@
 
 int main() {
     // Command to execute PowerShell
-    LPSTR command = "powershell -Command Get-Process | ?{$_.Name -eq 'test'} | Select-Object -ExpandProperty Id";
+    LPSTR command = "powershell -Command Get-Process | ?{$_.Name -eq ''} | Select-Object -ExpandProperty Id";
     //Need to write output from this to buffer
 
 
